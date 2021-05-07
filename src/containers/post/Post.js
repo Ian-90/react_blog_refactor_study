7df38f2bff97e8ee9c57d443527deaf2882d3039
LeeYoHan <yohan4551@gmail.com>
2021-05-08 00:57:26 +0900

    feat: post page redux thunk 및 immer 적용

--- src/containers/post/Post.js
+++ src/containers/post/Post.js
@@ -1,47 +1,36 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import PostInfo from 'components/post/PostInfo'
 import PostBody from 'components/post/PostBody'
-import * as postActions from 'store/modules/post'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
+import { getPost } from 'store/modules/post'
 
-class Post extends Component {
-  initialize = async () => {
-    const { PostActions, id } = this.props
-    try {
-      await PostActions.getPost(id)
-    } catch(e) {
-      console.log(e)
-    }
+const Post = ({ id }) => {
+  const { loading, post, error } = useSelector((state) => state.post)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPost(id))
+  }, [dispatch, id])
+
+  if (loading) {
+    return <div>loading...</div>
   }
 
-  componentDidMount = () => this.initialize()
-  render() {
-    const { loading, post } = this.props
-
-    if (loading) return null
-
-    const { title, body, pulishedDate, tags } = post.toJS()
-    return (
-      <div>
-        <PostInfo
-          title={title}
-          pulishedDate={pulishedDate}
-          tags={tags}
-        />
-        <PostBody body={body} />
-      </div>
-    )
-
+  if (error) {
+    return <div>error!!</div>
   }
+
+  const { title, body, pulishedDate, tags } = post
+  return (
+    <div>
+      <PostInfo
+        title={title}
+        pulishedDate={pulishedDate}
+        tags={tags}
+      />
+      <PostBody body={body} />
+    </div>
+  )
 }
 
-export default connect(
-  (state) => ({
-    post: state.post.get('post'),
-    loading: state.pender.pending['post/GET_POST']
-  }),
-  (dispatch) => ({
-    PostActions: bindActionCreators(postActions, dispatch)
-  })
-)(Post)
+export default Post
--- src/store/modules/post.js
+++ src/store/modules/post.js
@@ -1,24 +1,55 @@
-import { createAction, handleActions } from 'redux-actions'
-import { Map, fromJS } from 'immutable'
-import { pender } from 'redux-pender'
+import produce from 'immer'
 import * as api from 'lib/api'
 
 const GET_POST = 'post/GET_POST'
+const GET_POST_SUCCESS = 'post/GET_POST_SUCCESS'
+const GET_POST_ERROR = 'post/GET_POST_ERROR'
+
+// TODO: REMOVE modal 연동 필요
 const REMOVE_POST = 'post/REMOVE_POST'
 
-export const getPost = createAction(GET_POST, api.getPost)
-export const removePost = createAction(REMOVE_POST, api.removePost)
+export const getPost = (id) => async (dispatch) => {
+  dispatch({ type: GET_POST})
+  try {
+    const { data } = await api.getPost(id)
+    dispatch({ type: GET_POST_SUCCESS, payload: data })
+  } catch (error) {
+    dispatch({ type: GET_POST_ERROR, payload: error })
+  }
+}
 
-const initiaState = Map({
-  post: Map({})
-})
+const initialState = {
+  loading: false,
+  post: {},
+  error: null
+}
 
-export default handleActions({
-  ...pender({
-    type: GET_POST,
-    onSuccess: (state, action) => {
-      const { data: post } = action.payload
-      return state.set('post', fromJS(post))
-    }
-  })
-}, initiaState)
+const postReducer = produce((state, action) => {
+  switch(action.type) {
+    case GET_POST:
+      state.loading = true
+      return state
+    case GET_POST_SUCCESS:
+      state.loading = false
+      state.post = action.payload
+      return state
+    case GET_POST_ERROR:
+      state.loading = false
+      state.error = action.payload
+      return state
+    default:
+      return state
+  }
+}, initialState)
+
+export default postReducer
+
+// export default handleActions({
+//   ...pender({
+//     type: GET_POST,
+//     onSuccess: (state, action) => {
+//       const { data: post } = action.payload
+//       return state.set('post', fromJS(post))
+//     }
+//   })
+// }, initiaState)
